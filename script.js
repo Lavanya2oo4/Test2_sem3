eace178fd0f2cfeb6014a7cdb7ca1233bc9a6c5e
Lavanya2oo4 <lvsingh2004@gmail.com>
2024-01-24 14:59:56 -0500

    Added functions to display error if either of fields in invalid or blank

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
-let submit=document.getElementById("submit")   
+let submit=document.getElementById("submit") 
+let message=document.getElementById("message") 
+
+
+let name0=document.getElementById("name")
+let phone=document.getElementById("phone")
+
+name0.addEventListener("blur",()=>{
+
+    if(name0.value==""){
+      showError("Name cannot be empty")    
+      name0.style.boxShadow="2px 2px 2px red"
+
+      return
+    }
+
+    let res=validateName()
+    checkValid()
+    console.log(res)
+    if(res){
+       removeError()
+      name0.style.boxShadow="none"
+
+    }
+    else{
+        
+    showError("Error:First and Last Names are required!!")
+      name0.style.boxShadow="2px 2px 2px red"
+      
+    }    
+})
+phone.addEventListener("blur",()=>{
+    if(phone.value==""){
+        showError("Phone number cannot be empty")    
+        name0.style.boxShadow="2px 2px 2px red"
+  
+        return
+      }
+   let res= validateNum()
+    checkValid()
+    if(res){
+        removeError()
+       phone.style.boxShadow="none"
+ 
+     }
+     else{
+         
+     showError("Error:Phone number must have exactly 6 characters!!")
+       phone.style.boxShadow="2px 2px 2px red"
+       
+     }    
+
+
+})
+
+
 
 // disabling button by default
 submit.disabled=true
@@ -7,7 +62,8 @@ let result=false
 // validating name
 function validateName(){
 
-    let name=document.getElementById("name").value
+    let name=name0.value
+    
     let format=/^[A-Z]{1}[a-z]+\s{0,2}[A-Z]{1}[a-z]+$/
     //Lavanya Lavanya
     result=format.test(name)
@@ -16,15 +72,14 @@ function validateName(){
 
 //validating number
 function validateNum(){
-    let phone=document.getElementById("phone").value
-    let format=/^\(?\d{3}\)?\-?[a-z\dA-Z]{3}\-[a-z\dA-Z]{4}?\n{0,2}$/
-    result=format.test(phone)
-    console.log(result)
+    let phone_=phone.value
+    // let format=/^\(?\d{3}\)?\-?[a-z0-9A-Z]{3}\-?[a-z0-9A-Z]{4}?\n{0,2}$/
+    let format=/^\d{3}\-?[a-z0-9A-Z]{3}\n{0,2}$/
+    result=format.test(phone_)
     return result
 
 }
 
-let interval=setInterval(checkValid,500)
 
 
 // if both values are valid button gets enabled
@@ -34,7 +89,18 @@ function checkValid(){
         submit.disabled=false
         
     }
-    if(!submit.disabled){
-        clearInterval(interval)
+    else{
+        submit.disabled=true
     }
+    
+    
+}
+
+
+function showError(msg){
+    message.innerHTML=msg
+}
+
+function removeError(){
+    message.innerHTML=""
 }
